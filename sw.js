5fa9b61ad2bd54302e9783e214f9e33720c3ccdf
Anurag Kumar <65001074+anuragkumar19@users.noreply.github.com>
2021-11-08 17:30:20 +0530

    network first

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 // Files to cache
-const cacheName = 'vedicMaths-v1.0.2';
+const cacheName = 'vedicMaths-v1.0.1'
 
-const appShellFiles = ['https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.2/zephyr/bootstrap.min.css',
+const appShellFiles = [
+    'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.2/zephyr/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
     'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap',
     'https://fonts.gstatic.com/s/inter/v3/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7.woff2',
@@ -17,31 +18,60 @@ const appShellFiles = ['https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.2/
     '/bal.html',
     '/kishor.html',
     '/favicon.ico',
-    '/q.js', '/app.js',
-    '/question.html']
+    '/q.js',
+    '/app.js',
+    '/question.html',
+]
 
 // Installing Service Worker
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil((async () => {
-        const cache = await caches.open(cacheName);
-        console.log('[Service Worker] Caching all: app shell and content');
-        await cache.addAll(appShellFiles);
-    })());
-});
+    console.log('[Service Worker] Install')
+    e.waitUntil(
+        (async () => {
+            const cache = await caches.open(cacheName)
+            console.log('[Service Worker] Caching all: app shell and content')
+            await cache.addAll(appShellFiles)
+        })()
+    )
+})
 
 // Fetching content using Service Worker
 self.addEventListener('fetch', (e) => {
-    e.respondWith((async () => {
-        const r = await caches.match(e.request, {
-            ignoreSearch: true
-        });
-        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-        if (r) return r;
-        const response = await fetch(e.request);
-        const cache = await caches.open(cacheName);
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-        cache.put(e.request, response.clone());
-        return response;
-    })());
-});
+    e.respondWith(
+        (async () => {
+            // Network first
+            try {
+                const response = await fetch(e.request)
+                const cache = await caches.open(cacheName)
+                console.log(
+                    `[Service Worker] Caching new resource: ${e.request.url}`
+                )
+                cache.put(e.request, response.clone())
+                return response
+            } catch (err) {
+                const r = await caches.match(e.request, {
+                    ignoreSearch: true,
+                })
+                console.log(
+                    `[Service Worker] Fetching resource: ${e.request.url}`
+                )
+                if (r) return r
+            }
+
+            // Cache first
+
+            // const r = await caches.match(e.request, {
+            //     ignoreSearch: true,
+            // })
+            // console.log(`[Service Worker] Fetching resource: ${e.request.url}`)
+            // if (r) return r
+            // const response = await fetch(e.request)
+            // const cache = await caches.open(cacheName)
+            // console.log(
+            //     `[Service Worker] Caching new resource: ${e.request.url}`
+            // )
+            // cache.put(e.request, response.clone())
+            // return response
+        })()
+    )
+})
